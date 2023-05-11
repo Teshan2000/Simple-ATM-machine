5da7940f8020e79b72a3a919ee72dc7bf2fa205e
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-05-11 12:29:42 +0530

    Scanner class
    Scanner class was imported to get user inputs.

--- ATM_machine.java
+++ ATM_machine.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ATM_machine {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to the ATM Machine!");
+        System.out.println("Please Enter your Card Number:");
+        int cardNo = input.nextInt();
+
+        System.out.println("Please Enter your Pin Number: ");
+        int pinNo = input.nextInt();
+
+        
+
+
     }
 }

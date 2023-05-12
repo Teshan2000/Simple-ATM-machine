a752225a3d9855417d0c18602a74ed51076b5b0c
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-05-12 17:57:17 +0530

    Checking User Inputs
    If clauses were implemented to check eligibility of user inputs

--- ATM_machine.java
+++ ATM_machine.java
@@ -3,17 +3,39 @@ import java.util.Scanner;
 public class ATM_machine {
     public static void main(String[] args) {
 
+        String cardNo = "200020104110";
+        String pinNo = "1234";
+
         Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to the ATM Machine!");
+
         System.out.println("Please Enter your Card Number:");
-        int cardNo = input.nextInt();
+        String enteredCardNo = input.nextLine();
 
-        System.out.println("Please Enter your Pin Number: ");
-        int pinNo = input.nextInt();
-
+        if (enteredCardNo.equals(cardNo)) {
+            system();
+        } 
+        else {
+            System.out.println("Invalid Card Number!");
+            System.out.println("Please Enter Again!");
+        }
         
+        System.out.println("Please Enter your Pin Number: ");
+        String enteredPinNo = input.nextLine();
+
+        if (enteredPinNo.equals(pinNo)) {
+            system();
+        }        
+        else {
+            System.out.println("Invalid Pin Number!");
+            System.out.println("Please Enter Again!");
+        }
 
 
+    }
+
+    static void system() {
+        System.out.println("Please select your choice");
     }
 }

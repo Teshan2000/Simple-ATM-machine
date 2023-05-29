550162f62deea30c3ec24487ca685c07465e49fc
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-05-29 07:54:36 +0530

    Checking Card Number
    If user enter wrong card number 3 times the system will show an error, and loop back to the home.

--- ATM_machine.java
+++ ATM_machine.java
@@ -11,72 +11,82 @@ public class ATM_machine {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to the ATM Machine!");
+        while (cardCount<3) {
 
-        System.out.println("Please Enter your Card Number:");
-        String enteredCardNo = input.nextLine();
+            System.out.println("Welcome to the ATM Machine!");
 
-        if (!enteredCardNo.equals(cardNo)) {
-            System.out.println("Invalid Card Number!");
-            System.out.println("Please Enter Again!");
-            main(args);
-        } 
-        
-        else {
+            System.out.println("Please Enter your Card Number:");
+            String enteredCardNo = input.nextLine();
 
-            System.out.println("Please Enter your Pin Number: ");
-            String enteredPinNo = input.nextLine();
-
-            if (!enteredPinNo.equals(pinNo)) {                
-
-                System.out.println("Invalid Pin Number!");
+            if (!enteredCardNo.equals(cardNo)) {
+                System.out.println("Invalid Card Number!");
                 System.out.println("Please Enter Again!");
-                main(args);
-            }
+                cardCount++;
+                //main(args);
 
+                if (cardCount==3) {
+                    System.out.println("You entered your Card Number 3 Times!");
+                    System.out.println("Please Enter your Card Number Again!");   
+                    main(args);  
+                }  
+            } 
+            
             else {
 
-                System.out.println("Do You want to continue Transaction?");
-                System.out.println("\n\t\t\t\t (1)Yes");
-                System.out.println("\n\t\t\t\t (2)No");
-                int continueTransaction = input.nextInt();     
+                System.out.println("Please Enter your Pin Number: ");
+                String enteredPinNo = input.nextLine();
 
-                if (continueTransaction==1) {
+                if (!enteredPinNo.equals(pinNo)) {                
 
-                    System.out.println("Please Choose your option!");
-
-                    System.out.println("\n\t\t\t\t (1) Check your Balance");
-                    System.out.println("\n\t\t\t\t (2) Withdraw your Money");
-                    System.out.println("\n\t\t\t\t (3) Deposit your Money");
-                    System.out.println("\n\t\t\t\t (4) Cancel");
-                    int choice = input.nextInt();
-
-                    switch (choice) {
-
-                        case 1:
-                        Balance();
-                        break;
-
-                        case 2:
-                        Withdraw();
-                        break;
-
-                        case 3:
-                        Deposit();
-                        break;
-
-                        case 4:
-                        //exit();
-                        main(args);
-
-                    }                
+                    System.out.println("Invalid Pin Number!");
+                    System.out.println("Please Enter Again!");
+                    main(args);
                 }
 
-                if (continueTransaction==2) {
+                else {
 
-                    System.exit(continueTransaction);
-                }  
+                    System.out.println("Do You want to continue Transaction?");
+                    System.out.println("\n\t\t\t\t (1)Yes");
+                    System.out.println("\n\t\t\t\t (2)No");
+                    int continueTransaction = input.nextInt();     
 
+                    if (continueTransaction==1) {
+
+                        System.out.println("Please Choose your option!");
+
+                        System.out.println("\n\t\t\t\t (1) Check your Balance");
+                        System.out.println("\n\t\t\t\t (2) Withdraw your Money");
+                        System.out.println("\n\t\t\t\t (3) Deposit your Money");
+                        System.out.println("\n\t\t\t\t (4) Cancel");
+                        int choice = input.nextInt();
+
+                        switch (choice) {
+
+                            case 1:
+                            Balance();
+                            break;
+
+                            case 2:
+                            Withdraw();
+                            break;
+
+                            case 3:
+                            Deposit();
+                            break;
+
+                            case 4:
+                            //exit();
+                            main(args);
+
+                        }                
+                    }
+
+                    if (continueTransaction==2) {
+
+                        System.exit(continueTransaction);
+                    }  
+
+                }
             }
         }
 
